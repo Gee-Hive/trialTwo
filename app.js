b07af0a75b93ecb71825ef949981e676f83d2b0a
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2021-02-03 13:29:59 +0100

    add a block of code

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 console.log("Hello World");
 console.warn("Keep Practicing");
 console.error("KEEP PRACTICING");
+
+let clubList = [["man u", 50], ["chelsea", 45], ["barca", 37], ["napoli", 35], ["arsenal", 40 ], ["juventus", 35] ];
+
+function scoreSheet(val1, val2){
+    if(val1 > val2){
+        return "approved";
+    }else if(val1 < val2) {
+        return "not approved";
+    }else if(val1 === val2){
+        return "a tie";
+    }else{
+        return 'try again';
+    }
+};
+console.log(scoreSheet(clubList[-1], clubList[-3]));
+
+let 
